2484306a2b1361db13be5d654712aa6c4c6d3baf
Павел Мажин <pavelmazin502@gmail.com>
2026-03-12 14:56:47 +0700

    ,kz ,zk

---
+++ 1c.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+
+
+double perimetrtrap(double a, double b, double c, double d)
+{
+    return a+b+c+d;
+}
+
+double ploch(double a,double b, double h)
+{
+    return (a+b)/2.0 *h;
+}
+
+double midLine(double a,double b)
+{
+    return (a+b)/2.0;
+}
